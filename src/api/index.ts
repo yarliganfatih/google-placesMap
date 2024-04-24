3408b4ab39a3599fe490bbe44c213d0ea4883496
Fatih Yarlıgan <fatih-yarligan@outlook.com>
2024-04-25 02:08:33 +0300

    added backend middleware for APIs

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 export async function getPlaces(lat: number, lng: number, rad: number) {
-  const callURL = `${import.meta.env.VITE_PLACES_API_URL}?location=${lat},${lng}&radius=${rad}&key=${import.meta.env.VITE_GOOGLE_API_KEY}`;
+  const callURL = `${import.meta.env.VITE_PLACES_API_URL}?lat=${lat}&lng=${lng}&radius=${rad}`;
   const response = await fetch(callURL);
   if(!response.ok) return null;
   const data = await response.json();
@@ -7,7 +7,7 @@ export async function getPlaces(lat: number, lng: number, rad: number) {
 }
 
 export async function getPlacePhoto(referance: string) {
-  const callURL = `${import.meta.env.VITE_PLACE_PHOTO_API_URL}?photoreference=${referance}&maxheight=240&maxwidth=426&key=${import.meta.env.VITE_GOOGLE_API_KEY}`;
+  const callURL = `${import.meta.env.VITE_PLACE_PHOTO_API_URL}?photoreference=${referance}&maxheight=240&maxwidth=426`;
   const response = await fetch(callURL);
   if(!response.ok) return null;
   return response.url;
